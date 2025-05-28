ba842ac5409ab2bc327e135ce2baf01f4741cedc
andriomdac <andriomdac@gmail.com>
2025-05-27 21:48:57 -0300

    replace serialize_store_item for serialize_basic_store_item, for better performance

--- prices/serializers.py
+++ prices/serializers.py
@@ -1,4 +1,4 @@
-from store_items.serializers import serialize_store_item
+from store_items.serializers import serialize_basic_store_item
 
 def serialize_price(price):
     """
@@ -6,7 +6,7 @@ def serialize_price(price):
     """
     return {
         "id": price.pk,
-        "store_item": serialize_store_item(price.store_item),
+        "store_item": serialize_basic_store_item(price.store_item),
         "cost_price": price.cost_price,
         "selling_price": price.selling_price,
         "created_at": price.created_at,
