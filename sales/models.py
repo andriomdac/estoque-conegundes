34fb9b7c9ed8d4a9d2189e680ac1c859ab41e2e1
andriomdac <andriomdac@gmail.com>
2025-05-27 21:51:56 -0300

    add 'active' model fiel to Sale

--- sales/models.py
+++ sales/models.py
@@ -5,6 +5,7 @@ class Sale(models.Model):
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Venda {self.pk} criada em {self.created_at} - R$ {self.total_amount}"
