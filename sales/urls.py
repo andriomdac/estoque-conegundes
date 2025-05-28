823e5f6e38204000d5cab17752af98e7109a952a
andriomdac <andriomdac@gmail.com>
2025-05-27 21:53:56 -0300

    add finalize/activate sale endpoint

--- sales/urls.py
+++ sales/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import (
-    sale_create_list_view, sale_detail_delete_view
+    sale_create_list_view, sale_detail_delete_view, activate_sale, sale_activate_view, sale_finalize_view
 )
 
 
 urlpatterns = [
     path('', sale_create_list_view, name='sale_create_list'),
     path('<int:sale_id>/', sale_detail_delete_view, name='sale_update_detail_delete'),
+    path('activate/<int:sale_id>/', sale_activate_view, name='sale_activate'),
+    path('finalize/<int:sale_id>/', sale_finalize_view, name='sale_finalize'),
+
 
 ]
--- sales/utils.py
+++ sales/utils.py
@@ -1,8 +1,9 @@
 from .models import Sale
-from app.utils.http import build_json_response
+from app.utils.http import build_json_response, build_json_error_response
 from .serializers import serialize_sale
 from app.utils.exceptions import ObjectValidationError 
 from django.core.validators import ValidationError
+from icecream import ic
 
 def create_new_sale(request, response):
     """
@@ -13,13 +14,31 @@ def create_new_sale(request, response):
 
 
 def update_sale_total_amount(sale):
-    try:
-        payments_total = 0
-        for payment in sale.payment_methods.all():
-            payments_total += payment.total_amount 
-        
-        sale.total_amount = payments_total
-        sale.full_clean()
-        sale.save()
-    except ValidationError as e:
-        raise ObjectValidationError(e)
+	try:
+		payments_total = 0
+		for payment in sale.payment_methods.all():
+			payments_total += payment.total_amount 
+		
+		sale.total_amount = payments_total
+		sale.full_clean()
+		sale.save()
+	except ValidationError as e:
+		raise ObjectValidationError(e)
+
+
+def finalize_sale(response, sale_id):
+	sale = Sale.objects.get(id=sale_id)
+	sale.active = False # All deactivated sales (active=False) are finalized/finished sales
+
+	sale.full_clean()
+	sale.save() 
+	return build_json_response(response, serialize_sale(sale), 200)
+
+
+def activate_sale(response, sale_id):
+	sale = Sale.objects.get(id=sale_id)
+	sale.active = True # All activated sales (active=True) are open/unfinished sales
+
+	sale.full_clean()
+	sale.save()
+	return build_json_response(response, serialize_sale(sale), 200)
